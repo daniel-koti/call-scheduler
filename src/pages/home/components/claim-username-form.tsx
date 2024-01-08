2c6c86fe2ac16c767c0d23cf8e7be4e1122a4540
daniel-koti <danielkoti.moniz@gmail.com>
2024-01-08 14:56:11 +0100

    config: config prettier tailwindcs

--- src/pages/home/components/claim-username-form.tsx
+++ src/pages/home/components/claim-username-form.tsx
@@ -4,10 +4,10 @@ import { ArrowRight } from 'lucide-react'
 
 export function ClaimUsernameForm() {
   return (
-    <form className="grid grid-cols-[1fr_auto] gap-2 p-4 mt-4 bg-zinc-200 rounded-md">
+    <form className="mt-4 grid grid-cols-[1fr_auto] gap-2 rounded-md bg-zinc-200 p-4">
       <Input placeholder="call.com/usuario" />
       <Button className="col-auto bg-emerald-600 hover:bg-emerald-800">
-        Reservar <ArrowRight className="w-4 h-4 ml-2 " />
+        Reservar <ArrowRight className="ml-2 h-4 w-4 " />
       </Button>
     </form>
   )
