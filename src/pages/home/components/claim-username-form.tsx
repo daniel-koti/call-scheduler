3d86b2e0ba2ef18f95c6b0da5f34d5552557916b
daniel-koti <danielkoti.moniz@gmail.com>
2024-01-09 12:56:53 +0100

    feat: claim Username Form validation

--- src/pages/home/components/claim-username-form.tsx
+++ src/pages/home/components/claim-username-form.tsx
@@ -1,14 +1,61 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight } from 'lucide-react'
 
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O username precisa ter no mínimo 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O username pode ter apenas letras e hifens',
+    }) // precisa ser um caracter entre a-z, pode ter -
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <form className="mt-4 grid grid-cols-[1fr_auto] gap-2 rounded-md bg-zinc-200 p-4">
-      <Input placeholder="call.com/usuario" />
-      <Button className="col-auto bg-emerald-600 hover:bg-emerald-800">
-        Reservar <ArrowRight className="ml-2 h-4 w-4 " />
-      </Button>
-    </form>
+    <>
+      <form
+        onSubmit={handleSubmit(handlePreRegister)}
+        className="mt-4 grid grid-cols-[1fr_auto] gap-2 rounded-md bg-zinc-200 p-4"
+      >
+        <Input placeholder="call.com/seu-usuário" {...register('username')} />
+        <Button
+          type="submit"
+          className="col-auto bg-emerald-600 hover:bg-emerald-800"
+        >
+          Reservar <ArrowRight className="ml-2 h-4 w-4 " />
+        </Button>
+      </form>
+      <div className="mt-2">
+        {errors.username ? (
+          <span className="text-sm text-red-600">
+            {errors.username.message}
+          </span>
+        ) : (
+          <span className="text-sm text-zinc-500">
+            Digite o nome do usuário
+          </span>
+        )}
+      </div>
+    </>
   )
 }
