d5e37aaad050c78e40608552a5b6611a246c074e
daniel-koti <danielkoti.moniz@gmail.com>
2024-01-26 00:14:58 +0100

    feat: register time-interval on database

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth/next'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,7 +28,24 @@ export default async function handler(
     buildNextAuthOptions(req, res),
   )
 
-  return res.json({
-    session,
-  })
+  if (!session) {
+    return res.status(401).send({ message: 'User not authenticated' })
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
