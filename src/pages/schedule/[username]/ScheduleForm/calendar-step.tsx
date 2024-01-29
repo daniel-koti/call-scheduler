0f322298f9c55c2c82de3b91e979d21084c3aebc
daniel-koti <danielkoti.moniz@gmail.com>
2024-01-29 18:46:34 +0100

    feature: create time picker component

--- src/pages/schedule/[username]/ScheduleForm/calendar-step.tsx
+++ src/pages/schedule/[username]/ScheduleForm/calendar-step.tsx
@@ -1,10 +1,39 @@
 import { Calendar } from '@/components/calendar'
+import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Card className="relative mx-auto mb-0 mt-6 grid w-[540px] max-w-full grid-cols-1 p-0">
+    <Card
+      data-isTimePickerOpen={isDateSelected}
+      className="data-[isTimePickerOpen=false]:grid-cols-1fr relative mx-auto mb-0 mt-6 grid max-w-full grid-cols-1 p-0 data-[isTimePickerOpen=false]:w-[540px] data-[isTimePickerOpen=true]:grid-cols-[1fr_280px]"
+    >
       <Calendar />
+
+      {isDateSelected && (
+        <div className="absolute bottom-0 right-0 top-0 w-[280px] overflow-y-scroll border-l border-zinc-200 px-6 pb-0 pt-6">
+          <header className="text-sm font-medium text-zinc-500">
+            terça-feira <span className=" text-zinc-900">20 de Janeiro</span>
+          </header>
+          <div className="mt-3 grid grid-cols-2 gap-2 md:grid-cols-1">
+            <Button>08:00h</Button>
+            <Button>09:00h</Button>
+            <Button>10:00h</Button>
+            <Button>11:00h</Button>
+            <Button>12:00h</Button>
+            <Button>13:00h</Button>
+            <Button>14:00h</Button>
+            <Button>15:00h</Button>
+            <Button>16:00h</Button>
+            <Button>17:00h</Button>
+            <Button>18:00h</Button>
+            <Button>19:00h</Button>
+            <Button>20:00h</Button>
+          </div>
+        </div>
+      )}
     </Card>
   )
 }
