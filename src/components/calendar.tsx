c5771891962a0f68dd1e5b5df0fcd83c212b7cba
daniel-koti <danielkoti.moniz@gmail.com>
2024-02-03 19:10:28 +0100

    fix: blocked days validation

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -43,6 +43,10 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const { data: blockedDates } = useGetBlockedDays(username, dateParams)
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -83,7 +87,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
