c45ac8f20fd8acc1d41af3123c6f9bed599cfae2
daniel-koti <danielkoti.moniz@gmail.com>
2024-02-04 22:59:56 +0100

    feat: update database to mysql

---
+++ prisma/migrations/20240204214405_update_text_field_types/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `accounts` MODIFY `refresh_token` TEXT NULL,
+    MODIFY `access_token` TEXT NULL,
+    MODIFY `id_token` TEXT NULL;
+
+-- AlterTable
+ALTER TABLE `users` MODIFY `name` TEXT NOT NULL,
+    MODIFY `bio` TEXT NULL;
