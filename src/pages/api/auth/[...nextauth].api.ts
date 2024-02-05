ed938d0bce79e1cc64cbe7f5a4972f9e0c4f2db5
daniel-koti <danielkoti.moniz@gmail.com>
2024-02-05 12:07:03 +0100

    refactor: add google refresh token on account model

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -17,6 +17,9 @@ export function buildNextAuthOptions(
         clientSecret: env.GOOGLE_CLIENT_SECRET,
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
